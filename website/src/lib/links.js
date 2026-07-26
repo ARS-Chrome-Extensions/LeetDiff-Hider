f50213d87936550b041c04889af4ace9d1144cd6
atharvarajsinha <sinhaatharva@gmail.com>
2026-07-26 15:47:04 +0530

    updated repo links

--- website/src/lib/links.js
+++ website/src/lib/links.js
@@ -1,10 +1,10 @@
 export const GITHUB_REPO_URL =
-  "https://github.com/atharvarajsinha/LeetDiff-Hider";
+  "https://github.com/ARS-Chrome-Extensions/LeetDiff-Hider";
 
 export const GITHUB_URL =
   "https://github.com/atharvarajsinha";
 
 export const DOWNLOAD_ZIP_URL =
-  "https://github.com/atharvarajsinha/LeetDiff-Hider/releases/latest/download/extension.zip";
+  "https://github.com/ARS-Chrome-Extensions/LeetDiff-Hider/releases/latest/download/extension.zip";
 
 export const CHROME_STORE_URL = null;
